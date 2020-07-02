414ed0a64c5a8e8f6081c02afbc6a93fd833d7ec
kathir <khirshanth123@gmail.com>
2020-07-02 14:51:47 -0400

    Updates

---
+++ precache-manifest.709fd1066ddbba94679c51bbd40eb515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1907305c6df6a47820d9f63978e21938",
+    "url": "/khirshanth/todo.git/index.html"
+  },
+  {
+    "revision": "ae8ec2c50a854ad523b6",
+    "url": "/khirshanth/todo.git/static/css/main.84400a23.chunk.css"
+  },
+  {
+    "revision": "d6dd98baf7055825d21e",
+    "url": "/khirshanth/todo.git/static/js/2.a7271bd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/khirshanth/todo.git/static/js/2.a7271bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8ec2c50a854ad523b6",
+    "url": "/khirshanth/todo.git/static/js/main.7ffb7625.chunk.js"
+  },
+  {
+    "revision": "bbda950b0465ef936b15",
+    "url": "/khirshanth/todo.git/static/js/runtime-main.738cfb6b.js"
+  }
+]);
